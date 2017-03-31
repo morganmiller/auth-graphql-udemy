d96b577473a64e2b2256047cb116163636b99366
Morgan Miller <m.miller722@gmail.com>
2017-03-31 17:15:15 -0600

    configures Header to use CurrentUser query for rendering nav

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const {loading, user } = this.props.data;
+    if(loading) { return <div />; };
+
+    if(user) {
+      return <div>Logout</div>
+    } else {
+      return (
+        <div>
+          Signup/Signin
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
     );
   }
 }
 
-export default Header
+export default graphql(query)(Header);
