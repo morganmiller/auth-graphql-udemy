f579e6c4519848c43d21e10e6dfea431a6b854e4
Morgan Miller <m.miller722@gmail.com>
2017-03-31 17:37:24 -0600

    scaffolds login route/component

--- client/components/App.js
+++ client/components/App.js
@@ -5,7 +5,7 @@ const App = (props) => {
   return (
     <div>
       <Header />
-      {props.childern}
+      {props.children}
     </div>
   );
 };
---
+++ client/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        HI hello hellooooooo
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
