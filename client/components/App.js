3dc55072fe77f9a6e793aae2d1b9524e91588820
Morgan Miller <m.miller722@gmail.com>
2017-03-31 16:53:24 -0600

    creates Header component

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 const App = (props) => {
   return (
     <div>
+      <Header />
       {props.childern}
-      hiii
     </div>
   );
 };
